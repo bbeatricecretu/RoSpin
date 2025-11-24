10fb38cee00f5acbc77e61ae3890b42cf45c037e
bbeatricecretu <cretu.beatricedenisa@gmail.com>
2025-11-24 19:24:59 +0200

    db updated

--- skywind-frontend/src/dtos/RegionComputeResponseDTO.ts
+++ skywind-frontend/src/dtos/RegionComputeResponseDTO.ts
@@ -1,18 +1,18 @@
-export interface ZoneComputeDTO {
-  index: number;
-  A: { lat: number; lon: number };
-  B: { lat: number; lon: number };
-  C: { lat: number; lon: number };
-  D: { lat: number; lon: number };
-}
-
 export interface RegionComputeResponseDTO {
+  region_id: number;
   center: { lat: number; lon: number };
   corners: {
+    A: { lat: number; lon: number } | null;
+    B: { lat: number; lon: number } | null;
+    C: { lat: number; lon: number } | null;
+    D: { lat: number; lon: number } | null;
+  };
+  zones: {
+    id: number;
+    index: number;
     A: { lat: number; lon: number };
     B: { lat: number; lon: number };
     C: { lat: number; lon: number };
     D: { lat: number; lon: number };
-  };
-  zones: ZoneComputeDTO[];
+  }[];
 }
